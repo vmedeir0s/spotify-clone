54dde678cdc0614a1567cab86ea38ec3c0b5d9a3
Vinicius de Medeiros <90739386+vmedeir0s@users.noreply.github.com>
2023-07-17 17:04:50 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="h-screen flex flex-col">
+		<div className="max-h-screen flex flex-col">
 			<div className="flex flex-1">
 				<Sidebar />
 				<main className="flex-1 p-6 overflow-y-auto">
